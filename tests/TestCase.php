90d80b315922cab03bd751ca86d48f92ed0a07f6
Martin Kiesel <martin.kiesel@gmail.com>
2017-03-21 00:32:37 +0100

    Update testing suite

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,13 +2,32 @@
 
 namespace Tests;
 
+use Exception;
 use Laravel\BrowserKitTesting\TestCase as BaseTestCase;
+use Yap\Exceptions\Handler;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
 
-    public $baseUrl = 'http://yap.dev';
+    public $baseUrl = 'http://test.dev';
 
-    // ...
+    /**
+     * Disable Laravel's exception handling.
+     *
+     * @return $this
+     */
+    protected function disableExceptionHandling()
+    {
+        app()->instance(Handler::class, new class extends Handler {
+            public function __construct() {}
+            public function report(Exception $e) {}
+            public function render($request, Exception $e)
+            {
+                throw $e;
+            }
+        });
+
+        return $this;
+    }
 }
