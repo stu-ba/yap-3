8abc7d11a55a0e7a3efe58d6d68d672a0427549f
Martin Kiesel <martin.kiesel@gmail.com>
2017-03-21 19:20:03 +0100

    Add new helper function in_range

--- app/helpers.php
+++ app/helpers.php
@@ -12,3 +12,24 @@ if (! function_exists('d')) {
         }, func_get_args());
     }
 }
+
+if (! function_exists('in_range')) {
+    /**
+     * Determines if $number is between $min and $max
+     *
+     * @param  integer $number    The number to test
+     * @param  integer $min       The minimum value in the range
+     * @param  integer $max       The maximum value in the range
+     * @param  boolean $inclusive Whether the range should be inclusive or not
+     *
+     * @return boolean              Whether the number was in the range
+     */
+    function in_range($number, $min, $max, $inclusive = false)
+    {
+        if (is_int($number) && is_int($min) && is_int($max)) {
+            return $inclusive ? ($number >= $min && $number <= $max) : ($number > $min && $number < $max);
+        }
+
+        return false;
+    }
+}
