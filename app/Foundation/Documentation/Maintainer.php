7e1af1ab285bf2ef501580c81749071068668d6a
Martin Kiesel <martin.kiesel@gmail.com>
2017-04-02 02:41:36 +0200

    Add documentation maintainer
     - can install from repository (configured by documentation.php)
     - can update from repository
     - add unit tests

---
+++ app/Foundation/Documentation/Maintainer.php
@@ -0,0 +1,155 @@
+<?php
+
+namespace Yap\Foundation\Documentation;
+
+use Illuminate\Filesystem\Filesystem;
+use Yap\Exceptions\DocumentationException;
+
+class Maintainer
+{
+
+    const THROW = 0x1;
+    const SKIP_EXISTS = 0x2;
+    const SKIP_DIRECTORY = 0x4;
+    const SKIP_WRITABLE = 0x8;
+
+    /**
+     * The filesystem implementation.
+     *
+     * @var Filesystem $files
+     */
+    protected $files;
+
+
+    /**
+     * Create a new documentation instance.
+     *
+     * @param  Filesystem $files
+     *
+     * @throws DocumentationException
+     */
+    public function __construct(Filesystem $files)
+    {
+        $this->files = $files;
+
+        $path = config('documentation.path');
+
+        if (str_contains($path, '.')) {
+            throw new DocumentationException('Path to documentation can not contain \'.\'.');
+        } elseif ( ! str_contains(config('documentation.git.repository'), 'github.com')) {
+            throw new DocumentationException('Documentation repository must be hosted on github.com.');
+        }
+
+        $this->path = base_path($path);
+    }
+
+
+    /**
+     * Installs documentation repository.
+     *
+     * @param bool $force
+     *
+     * @return bool
+     * @throws DocumentationException
+     */
+    public function install(bool $force = false): bool
+    {
+        if ( ! $force && $this->check()) {
+            throw new DocumentationException('Repository already exists.', 1);
+        }
+
+        if ($force) {
+            $this->clean();
+        }
+
+        $this->files->makeDirectory($this->path, 0755, true, true);
+        $this->gitignore();
+        $this->clone();
+
+        return true;
+    }
+
+
+    public function check(int $flags = 0): bool
+    {
+        $throw = function ($message, $code) use ($flags) {
+            if ($flags & self::THROW) {
+                throw new DocumentationException($message, $code);
+            }
+
+            return false;
+        };
+
+        if ( ! ($flags & self::SKIP_EXISTS) && ! $this->files->exists($this->path.'/.git')) {
+            return $throw('Directory ['.$this->path.'] does not exist.', 2);
+        } elseif ( ! ($flags & self::SKIP_DIRECTORY) && ! $this->files->isDirectory($this->path)) {
+            return $throw('['.$this->path.'] is not directory.', 3);
+        } elseif ( ! ($flags & self::SKIP_WRITABLE) && ! $this->files->isWritable($this->path)) {
+            return $throw('Directory ['.$this->path.'] is not writable.', 4);
+        }
+
+        return true;
+    }
+
+
+    protected function clean(): bool
+    {
+        return $this->files->cleanDirectory($this->path);
+    }
+
+
+    /**
+     * Generates .gitignore file.
+     * If .gitignore exists it checks if entry is already in place.
+     */
+    protected function gitignore(): void
+    {
+        $dirname = $this->files->dirname($this->path);
+        $basename = $this->files->basename($this->path);
+
+        $gitignore = $dirname.'/.gitignore';
+        $gitignoreEntry = '/'.$basename."/\n";
+
+        if ($this->files->exists($gitignore)) {
+            if ( ! str_contains($this->files->get($gitignore), $gitignoreEntry)) {
+                $this->files->append($gitignore, $gitignoreEntry);
+            }
+        } else {
+            $this->files->put($gitignore, $gitignoreEntry);
+        }
+    }
+
+
+    protected function clone(): void
+    {
+        @exec('cd '.$this->path.' && '.$this->cloneCommand());
+    }
+
+
+    private function cloneCommand(): string
+    {
+        return 'git clone --branch '.config('documentation.git.branch').' --depth '.config('documentation.git.depth').' '.config('documentation.git.repository').' .';
+    }
+
+
+    public function update(): bool
+    {
+        $this->check(self::THROW);
+        $this->pull();
+
+        return true;
+    }
+
+
+    protected function pull(): void
+    {
+        @exec('cd '.$this->path.' && '.$this->pullCommand());
+    }
+
+
+    private function pullCommand(): string
+    {
+        return 'git pull origin '.config('documentation.git.branch');
+    }
+
+}
---
+++ tests/Unit/Documentation/MaintainerTest.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Tests\Unit\Documentation;
+
+use Illuminate\Filesystem\Filesystem;
+use Tests\TestCase;
+use Yap\Exceptions\DocumentationException;
+use Yap\Foundation\Documentation\Maintainer as MaintainerOriginal;
+
+class MaintainerTest extends TestCase
+{
+
+    /**
+     * @var Maintainer $maintainer
+     */
+    protected $maintainer;
+
+
+    /**
+     *
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        $this->maintainer = resolve(Maintainer::class);
+        $this->maintainer->setPath('tests/Unit/Documentation/Stubs/docs/');
+    }
+
+
+    public function testGitignoreIsAdded()
+    {
+        /** @var Filesystem $files */
+        $files = resolve(\Illuminate\Filesystem\Filesystem::class);
+        $this->maintainer->setPath('tests/Unit/Documentation/Stubs/downloads/');
+        $this->maintainer->gitignore();
+        $this->assertTrue($files->exists(base_path('tests/Unit/Documentation/Stubs/')));
+        $files->deleteDirectory(base_path('tests/Unit/Documentation/Stubs/.gitignore'));
+    }
+
+    public function testGitignoreIsUpdated() {
+        /** @var Filesystem $files */
+        $files = resolve(\Illuminate\Filesystem\Filesystem::class);
+        $gitignore = base_path('tests/Unit/Documentation/Stubs/.gitignore');
+        $this->maintainer->setPath('tests/Unit/Documentation/Stubs/downloads/');
+        $files->put($gitignore, "/joe\n");
+        $this->maintainer->gitignore();
+
+        $this->assertEquals("/joe\n/downloads/\n", $files->get($gitignore));
+        $files->deleteDirectory($gitignore);
+    }
+
+
+    public function testInstall()
+    {
+        $this->markTestSkipped('Test uses \'git clone\', run only if needed.');
+        /** @var Filesystem $files */
+        $files = resolve(\Illuminate\Filesystem\Filesystem::class);
+        $this->maintainer->setPath('tests/Unit/Documentation/Stubs/downloads/');
+        $this->maintainer->install();
+
+        $this->assertTrue($files->exists(base_path('tests/Unit/Documentation/Stubs/downloads/.git')));
+        $this->assertTrue($files->exists(base_path('tests/Unit/Documentation/Stubs/.gitignore')));
+
+        //clean up
+        $files->deleteDirectory(base_path('tests/Unit/Documentation/Stubs/downloads'));
+        $files->deleteDirectory(base_path('tests/Unit/Documentation/Stubs/.gitignore'));
+    }
+
+
+    public function testPull()
+    {
+        $this->markTestSkipped('Test uses \'git pull\', run only if needed.');
+        $this->maintainer->setPath('tests/Unit/Documentation/Stubs/downloads/');
+        $this->maintainer->install();
+        $this->maintainer->update();
+        //TODO: some assertion... not sure
+    }
+
+
+    public function testCheckThrows()
+    {
+        $this->maintainer->setPath(str_random(32));
+        $this->expectException(DocumentationException::class);
+        $this->expectExceptionCode(2);
+        $this->maintainer->check(Maintainer::THROW);
+    }
+
+
+    public function testCheckSkipsExists()
+    {
+        $this->maintainer->setPath(str_random(32));
+        $this->expectException(DocumentationException::class);
+        $this->expectExceptionCode(3);
+        $this->maintainer->check(Maintainer::THROW | Maintainer::SKIP_EXISTS);
+    }
+
+
+    public function testCheckSkipsDirectory()
+    {
+        $this->maintainer->setPath(str_random(32));
+        $this->expectException(DocumentationException::class);
+        $this->expectExceptionCode(4);
+        $this->maintainer->check(Maintainer::THROW | Maintainer::SKIP_EXISTS | Maintainer::SKIP_DIRECTORY);
+    }
+
+
+    public function testCheckSkipsWritable()
+    {
+        $this->maintainer->setPath(str_random(32));
+        $this->maintainer->check(Maintainer::SKIP_EXISTS | Maintainer::SKIP_DIRECTORY | Maintainer::SKIP_WRITABLE);
+    }
+}
+
+class Maintainer extends MaintainerOriginal
+{
+
+    //For testing purpose only
+    public function setPath($path)
+    {
+        $this->path = base_path($path);
+    }
+
+
+    public function gitignore(): void
+    {
+        parent::gitignore();
+    }
+}
