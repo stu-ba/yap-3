c088f0de327fb16ff92111917443780f3b411a67
Martin Kiesel <martin.kiesel@gmail.com>
2017-04-16 11:47:35 +0200

    Remove author meta tag

--- resources/views/layouts/documentation.blade.php
+++ resources/views/layouts/documentation.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <meta name="author" content="Martin Kiesel">
     <meta name="description" content="Yap - User Guide">
     <meta name="keywords" content="yap, yap-3, user guide">
     <meta name="viewport" content="width=device-width, initial-scale=1">
