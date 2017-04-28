2401bde2396806004d5b6b815ddc831abdd3cc18
Martin Kiesel <martin.kiesel@gmail.com>
2017-04-28 02:42:38 +0200

    Add html component, taiga button

---
+++ resources/views/components/html/taiga-button.blade.php
@@ -0,0 +1,1 @@
+<a href="{{ $href ?? '#' }}" class="{{ $class ?? 'btn btn-default' }}" rel="tooltip" data-placement="{{ $position ?? 'top' }}" title="{{ $tooltip ?? '' }}"><i>{!! svg('taiga-icon', 'taiga-icon fa-lg') !!}</i></a>
